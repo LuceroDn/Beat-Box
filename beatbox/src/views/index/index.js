a3b8410c31ed9750fe3ceaa38d55db80c16214a3
Lucero Dn <lucero_dn@hotmail.es>
2019-07-08 20:19:25 -0500

    Se conecto el componente index.js con redux

--- beatbox/src/views/index/index.js
+++ beatbox/src/views/index/index.js
@@ -1,46 +1,65 @@
 import React, { Component } from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
-import './index.css'
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+//import SongItem from './SongItem';
+
+import './index.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 class Index extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
+        this.state = {
             song: ""
         }
     }
     render() {
+        console.log(this.props);
         return (
             <div className="index">
                 <div className="card">
                     <div className="card-content">
                         <div className="index-searchBox">
 
-                            <input 
-                            type="text" 
-                            className="index-searchBox-input" 
-                            placeholder="Cancion" 
-                            onChange={(e) => {this.setState({song: e.target.value})}} 
-                            value={this.state.song} 
+                            <input
+                                type="text"
+                                className="index-searchBox-input"
+                                placeholder="Cancion"
+                                onChange={(e) => { this.setState({ song: e.target.value }) }}
+                                value={this.state.song}
                             />
                             <a href="hola" className="waves-effect waves-light btn green">
-                            <i className="fa fa-search"></i>
+                                <i className="fa fa-search"></i>
                             </a>
-                        
-                            </div>
+
+                        </div>
                     </div>
                 </div>
 
                 <div className="card index-results-card">
-                            <div className="card-content">
+                    <div className="card-content">
 
-                            </div>
-                            </div>
+                    </div>
+                </div>
 
             </div>
         );
     }
 }
 
-export default Index;
+function mapStateToProps(state) {
+    return {
+        routes: state.routes
+    }
+}
+
+//Se encarga de enviar el action al store
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
