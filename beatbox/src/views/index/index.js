820019970656cf1ff42d0c4d9e404d4e864c46cf
Lucero Dn <lucero_dn@hotmail.es>
2019-07-08 19:24:36 -0500

    Se creo componente SongsItem.js y se agrego card a index.js

--- beatbox/src/views/index/index.js
+++ beatbox/src/views/index/index.js
@@ -1,14 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import './index.css'
 
 
 class Index extends Component {
-  render(){
-    return (
-<div>
-    Index
-    </div>
-      );
+    constructor(){
+        super();
+        this.state={
+            song: ""
+        }
+    }
+    render() {
+        return (
+            <div className="index">
+                <div className="card">
+                    <div className="card-content">
+                        <div className="index-searchBox">
+
+                            <input 
+                            type="text" 
+                            className="index-searchBox-input" 
+                            placeholder="Cancion" 
+                            onChange={(e) => {this.setState({song: e.target.value})}} 
+                            value={this.state.song} 
+                            />
+                            <a href="hola" className="waves-effect waves-light btn green">
+                            <i className="fa fa-search"></i>
+                            </a>
+                        
+                            </div>
+                    </div>
+                </div>
+
+                <div className="card index-results-card">
+                            <div className="card-content">
+
+                            </div>
+                            </div>
+
+            </div>
+        );
     }
 }
 
